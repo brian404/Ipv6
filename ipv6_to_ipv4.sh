cc41dd94cd26ac1409e67d344653ac283c7be7fb
brian <72920128+brian404@users.noreply.github.com>
2023-09-12 22:24:27 +0300

    Update ipv6_to_ipv4.sh

--- ipv6_to_ipv4.sh
+++ ipv6_to_ipv4.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-read -p "Enter IPv4 proxy address: " PROXY_IPV4
-
-while [[ ! $PROXY_IPV4 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; do
-  echo "Invalid IPv4 address format. Please try again."
-  read -p "Enter IPv4 proxy address: " PROXY_IPV4
-done
-
-read -p "Enter proxy port: " PROXY_PORT
-
-while ! [[ $PROXY_PORT =~ ^[0-9]+$ ]]; do
-  echo "Invalid port number format. Please try again."
-  read -p "Enter proxy port: " PROXY_PORT
-done
-
-# Install proxychains if not already installed
-if ! command -v proxychains &> /dev/null; then
-  echo "Installing proxychains..."
-  apt-get update
-  apt-get install -y proxychains
+# Get IPv4 proxy info
+read -p "Enter public IPv4 proxy address: " IPV4_PROXY
+if [[ ! $IPV4_PROXY =~ ^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; then
+  echo "Invalid public IP address format. Please try again."
+  exit 1
 fi
 
-# Set up proxychains configuration
-echo "socks5 $PROXY_IPV4 $PROXY_PORT" > /etc/proxychains.conf
+read -p "Enter proxy port: " PORT
+if [[ ! $PORT =~ ^[0-9]+$ ]] || [[ $PORT -lt 1 ]] || [[ $PORT -gt 65535 ]]; then
+  echo "Invalid port number. Please enter a number between 1 and 65535."
+  exit 1
+fi
 
-# Command to run IPv4 scripts or applications
-IPv4_COMMAND="your_ipv4_script.sh"  # Replace with the command for running your IPv4 script
+# Install proxychains if needed
+if ! command -v /usr/bin/proxychains >/dev/null 2>&1; then
+  echo "Installing proxychains..."
+  
+  apt-get update
+  apt-get install -y proxychains
+  
+  if [[ $? -ne 0 ]]; then
+    echo "Error installing proxychains"
+    exit 1
+  fi
+else
+  echo "proxychains already installed"
+fi
 
-# Run IPv4 script using the IPv6-to-IPv4 proxy
-proxychains $IPv4_COMMAND
+# Set up proxychains config
+echo "Using proxy: $IPV4_PROXY $PORT"
+echo "socks5 $IPV4_PROXY $PORT" > /etc/proxychains.conf
 
+if [[ $? -ne 0 ]]; then
+  echo "Error writing proxychains config"
+  exit 1
+fi
+
+# Run commands through proxy
+PROXY_CMD="/usr/bin/proxychains"
+
+echo "Proxy setup completed. Use '$PROXY_CMD' before commands to route through proxy."
+
+# Example:
+$PROXY_CMD ping 8.8.8.8
